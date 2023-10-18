d6b04ffd9b64566e19d4e931ea9c6ffcc0d21d62
Briana Jade Adricula <88810879+Brianadrix@users.noreply.github.com>
2023-10-18 15:35:12 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:upatrol/landing_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LandingPage(), 
+    );
+  }
+}
